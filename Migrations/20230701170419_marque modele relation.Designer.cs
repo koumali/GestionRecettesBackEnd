972720a3b72bbe602f22c64b89e03e0240849f3c
zakaria <you@example.com>
2023-07-01 18:06:11 +0100

    marque modele relationshi[

--- Migrations/20230701170419_marque modele relation.Designer.cs
+++ Migrations/20230701170419_marque modele relation.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using automotiveApi.DAL;
 
@@ -10,9 +11,11 @@ using automotiveApi.DAL;
 namespace automotive_api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230701170419_marque modele relation")]
+    partial class marquemodelerelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
