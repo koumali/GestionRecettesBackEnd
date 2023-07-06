271699a18a6c3daf263061a1dde5e47da79fadbc
koumali <105060044+koumali@users.noreply.github.com>
2023-07-06 17:54:42 +0100

    Update OffreService.cs

--- Services/Gestion/OffreService.cs
+++ Services/Gestion/OffreService.cs
@@ -8,7 +8,6 @@ namespace AutomotiveApi.Services.Gestion
     public class OffreService : GenericDataService<Offre>, IOffre
     {
         private readonly AppDbContext _context;
-
         public OffreService(AppDbContext context) : base(context)
         {
             _context = context;
@@ -29,21 +28,23 @@ namespace AutomotiveApi.Services.Gestion
         // }
         //
         //
-        // public Offre? findById(int id)
-        // {
-        //     var Offre = _context.Offres.Where(u => u.id == id).FirstOrDefault();
-        //     return Offre;
-        // }
-        //
-        //
+         public new async Task<Offre?> GetByIdAsync(int id)
+         {
+             return await _context.Offres.Where(u => u.Id == id)
+                                        .Include(o=>o.Vehicule.Agence)
+                                        .Include(o => o.Vehicule.Modele)
+                                            .ThenInclude(m => m.Marque)
+                                        .FirstOrDefaultAsync(); ;
+         }
+        
+        
         public new async Task<IEnumerable<Offre>> GetAllAsync()
-        {
-            return await _context.Offres.Include(o => o.Vehicule).ToListAsync();
+         {
+             return await _context.Offres.Include(o => o.Vehicule.Agence)
+                                          .Include(o => o.Vehicule.Modele)
+                                              .ThenInclude(m => m.Marque)
+                                          .ToListAsync();
         }
-        // public IEnumerable<Offre> getOffres()
-        // {
-        //     return _context.Offres.Include(o => o.Vehicule).ToList();
-        // }
         //
         // public void delete(int id)
         // {
