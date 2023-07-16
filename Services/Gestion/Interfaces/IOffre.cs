41ca44ad887fec7a57ab7e4551e5a880ac0a2c07
koumali <koumali2310@gmail.com>
2023-07-16 16:17:45 +0100

    search offre

--- Services/Gestion/Interfaces/IOffre.cs
+++ Services/Gestion/Interfaces/IOffre.cs
@@ -1,3 +1,4 @@
+using AutomotiveApi.Models.Dto;
 using AutomotiveApi.Models.Entities.Gestion;
 
 namespace AutomotiveApi.Services.Gestion.Interfaces
@@ -5,7 +6,7 @@ namespace AutomotiveApi.Services.Gestion.Interfaces
     public interface IOffre : IGenericData<Offre>
     {
         Task<IEnumerable<Offre>> GetOffresAgence(int idAgence);
-        Task<IEnumerable<Offre>> GetPublicOffres();
+        Task<IEnumerable<Offre>> GetPublicOffres(string name, DateTime datedebut, DateTime datefin);
         Task<Offre?> GetPublicByIdAsync(int id);
         // Offre? findById(int id);
         // Offre? add(Offre Offre);
