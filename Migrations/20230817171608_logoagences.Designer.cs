092a417ce2c45f9fbded7af221da5e5bb9f81417
koumali <koumali2310@gmail.com>
2023-08-17 21:14:10 +0100

    logos...

--- Migrations/20230817171608_logoagences.Designer.cs
+++ Migrations/20230817171608_logoagences.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using AutomotiveApi.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AutomotiveApi.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230817171608_logoagences")]
+    partial class logoagences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Dto/AgenceDto.cs
+++ Models/Dto/AgenceDto.cs
@@ -1,17 +1,22 @@
-using System.ComponentModel.DataAnnotations;
+using AutomotiveApi.Utility.Validators;
 
 namespace AutomotiveApi.Models.Dto;
 
 public class AgenceDto
 {
-    [Required] [MaxLength(50)] public string Name { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
     public string Tel { get; set; }
     public string Email { get; set; }
     public string Address { get; set; }
     public string City { get; set; }
     public string ZipCode { get; set; }
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
     public bool IsVerified { get; set; }
     public int? ParentId { get; set; }
+
+    [AllowedExtensions(new string[] { ".jpg", ".png", ".jpeg" })]
+    [MaxFileSize(1 * 1024 * 1024)]
+    public IFormFile? Logo { get; set; }
 }
