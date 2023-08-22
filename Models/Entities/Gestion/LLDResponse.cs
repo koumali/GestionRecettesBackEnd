482acab79459c72373c81d4a03e5e7b14ec5cc96
Hamza Lemsaoui <hamzalemsaoui@gmail.com>
2023-08-23 00:13:18 +0100

    client lldrseponses

--- Models/Entities/Gestion/LLDResponse.cs
+++ Models/Entities/Gestion/LLDResponse.cs
@@ -7,10 +7,10 @@ public class LLDResponse : ModelBase
     public int prix { get; set; }
     public string title { get; set; }
     public string description { get; set; }
-    public int idAgence { get; set; }
+    public int? idAgence { get; set; }
     public int idLongTermRental { get; set; }
     public bool isAgence { get; set; }
-    public virtual Agence Agence { get; set; }
+    public virtual Agence? Agence { get; set; }
     public virtual LongTermRental LongTermRental { get; set; }
     public ICollection<PieceJointes> PieceJointes { get; set; } = new List<PieceJointes>();
 }
