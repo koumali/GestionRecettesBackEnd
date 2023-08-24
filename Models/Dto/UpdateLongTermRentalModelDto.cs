d4369f7ec5846994464c8136615daa302eabf274
koumali <koumali2310@gmail.com>
2023-08-24 20:44:05 +0100

    lld...

--- Models/Dto/UpdateLongTermRentalModelDto.cs
+++ Models/Dto/UpdateLongTermRentalModelDto.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AutomotiveApi.Utility;
+using System.ComponentModel.DataAnnotations;
 
 namespace AutomotiveApi.Models.Dto;
 
@@ -6,8 +7,14 @@ public class UpdateLongTermRentalModelDto
 {
     public int Id { get; set; }
 
-    [Required(ErrorMessage = "l'agence est obligatoire")]
-    public int IdAgence { get; set; }
+    public int? IdAgence { get; set; }
+
+    [Required(ErrorMessage = "la duree est obligatoire")]
+    public int Duree { get; set; }
+
+    [Required(ErrorMessage = "le statut est obligatoire")]
+    [EnumDataType(typeof(ReservationStatus), ErrorMessage = "le statut n'est pas valide")]
+    public string Status { get; set; }
 
     [Required(ErrorMessage = "le montant total est obligatoire")]
     [Range(0, double.MaxValue, ErrorMessage = "le montant total doit être supérieur à 0")]
